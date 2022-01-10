f4f7a748d3dd598ac7ea1c8cbcda209d5f2a1ea9
shireen30 <70129608+shireen30@users.noreply.github.com>
2022-01-10 15:53:26 +0530

    Update ground.js

--- ground.js
+++ ground.js
@@ -1,17 +1,18 @@
-class ground  {
-    constructor(x,y,w,h)
-    {
-        
-        var x,y,w,h;
-        this.x=x
-        this.y = y
-        this.w = w
-        this.h = h
-        this.body = Bodies.rectangle(x,y,w,h)
-        World.add(world,this.body)
-    }
-    display()
-    {
-        rect(this.x,this.y,this.w,this.h)
-    }
+class ground {
+  constructor(x, y, w, h) {
+      var ground_options = {
+          isStatic : true,
+      }
+    var x, y, w, h;
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.body = Bodies.rectangle(x, y, w, h,ground_options);
+    console.log(this.body)
+    World.add(world, this.body);
+  }
+  display() {
+    rect(this.body.position.x, this.body.position.y, this.w, this.h);
+  }
 }
